f84b4bf81fcec6c260151bae39220fc013afd7f7
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-06-12 01:23:19 +0300

    sql-temelleri odev5 created

--- SQL/SQL Temelleri II/odev5.sql
+++ SQL/SQL Temelleri II/odev5.sql
@@ -4,7 +4,7 @@ Merhabalar,
 
 Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
 
-1- film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+1 - film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
 
 2- film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
 
