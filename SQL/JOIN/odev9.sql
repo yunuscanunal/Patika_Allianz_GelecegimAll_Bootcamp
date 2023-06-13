87d49d304e8899799890051d87632eca48c41d5c
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-06-13 12:24:10 +0300

    sql-join odev9 done

--- SQL/JOIN/odev9.sql
+++ SQL/JOIN/odev9.sql
@@ -14,9 +14,19 @@ Kolay Gelsin.
 */
 
 --1--
-
+SELECT city, country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
 
 --2--
-
+SELECT payment.payment_id, 
+	customer.first_name, 
+	customer.last_name 
+FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id;
 
 --3--
+SELECT rental.rental_id, 
+	customer.first_name, 
+	customer.last_name 
+FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id
