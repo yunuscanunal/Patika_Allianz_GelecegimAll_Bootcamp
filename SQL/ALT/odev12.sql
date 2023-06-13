f9f0265c537f2a72c81b8d23200ce73241c5636c
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-06-13 12:57:10 +0300

    sql-alt odev12 created

---
+++ SQL/ALT/odev12.sql
@@ -0,0 +1,24 @@
+/*
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1- film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+
+2- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+
+3- film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+
+4- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+Kolay Gelsin.
+*/
+
+--1
+
+
+--2
+
+--3
+
+--4
