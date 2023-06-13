4ee0a82e841c8981d48dc0ba66ead87578f5b4bb
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-06-13 12:31:57 +0300

    sql-join odev10 done

--- SQL/JOIN/odev10.sql
+++ SQL/JOIN/odev10.sql
@@ -13,9 +13,19 @@ Kolay Gelsin.
 */
 
 --1--
-
+SELECT country, city FROM country
+LEFT JOIN city ON city.country_id = country.country_id;
 
 --2--
-
+SELECT payment.payment_id, 
+	customer.first_name, 
+	customer.last_name 
+FROM customer
+LEFT JOIN payment ON customer.customer_id = payment.customer_id;
 
 --3--
+SELECT rental.rental_id, 
+	customer.first_name, 
+	customer.last_name 
+FROM customer
+LEFT JOIN rental ON customer.customer_id = rental.customer_id
