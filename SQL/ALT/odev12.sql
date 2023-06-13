bc463d465d2e624f22aaa8b6a142609424cf2002
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-06-13 13:28:17 +0300

    sql-alt odev12 done

--- SQL/ALT/odev12.sql
+++ SQL/ALT/odev12.sql
@@ -15,10 +15,33 @@ Kolay Gelsin.
 */
 
 --1
-
+SELECT length FROM film
+WHERE length > (
+	SELECT AVG(length) FROM film
+);
 
 --2
+SELECT rental_rate FROM film
+WHERE rental_rate = (
+	SELECT MAX(rental_rate) FROM film
+);
 
 --3
+SELECT rental_rate, replacement_cost FROM film
+WHERE (rental_rate = (
+	SELECT MIN(rental_rate) FROM film
+	)
+	AND replacement_cost = (
+	SELECT MIN(replacement_cost) FROM film
+	)
+);
 
 --4
+SELECT customer.customer_id, first_name, last_name, purchase FROM customer
+INNER JOIN (
+	SELECT COUNT(*) purchase, customer_id FROM payment
+	GROUP BY customer_id
+)
+count_purchases ON customer.customer_id = count_purchases.customer_id
+ORDER BY purchase DESC
+;
