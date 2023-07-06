8c83f3a9ea78c997dfdd5f2215f5ffd813f96418
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-07 01:43:43 +0300

    fixed

--- Java/projects/setup-intellij-jdk/src/Main.java
+++ Java/projects/setup-intellij-jdk/src/Main.java
@@ -2,16 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        // Press Ctrl+F5 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Opt+F5 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing F9.
-            System.out.println("i = " + i);
-        }
+        System.out.println("hello world");
     }
 }
