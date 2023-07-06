932ea2843332ce246e253da5aa52f09068f3c21e
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-07 00:25:18 +0300

    java lecture notes updated

--- Java/projects/setup-intellij-jdk/src/Main.java
+++ Java/projects/setup-intellij-jdk/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         // Press Ctrl+F5 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
