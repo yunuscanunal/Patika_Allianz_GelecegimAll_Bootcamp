2c0dd1ca16cf45374de86dc90c4e173ea11afa27
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-08 21:47:54 +0300

    javaIntro codes added

---
+++ Java/JavaBaslangic/javaIntro.java
@@ -0,0 +1,13 @@
+public class javaIntro {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        System.out.println("hello world");
+
+        System.out.println("Hello\nWorld");
+
+        System.out.println("Hello\tWorld");
+
+        System.out.println("Hello\bWorld");
+    }
+}
