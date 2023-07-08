d226efdc43966b6f5418ed1bf3cd409eb851105c
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-08 22:02:39 +0300

    javaVariables file updated and code file added

---
+++ Java/basicConcepts-Variables/javaVariables.java
@@ -0,0 +1,10 @@
+public class javaVariables {
+    public static void main(String[] args) {
+        int numberOne, numberTwo = 4;
+        int numberThree;
+        numberOne = 2;
+        numberThree = numberOne + numberTwo;
+
+        System.out.println(numberThree);
+    }
+}
