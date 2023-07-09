4287b372f187676b53725b5ab053def6fb71beeb
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-09 12:12:51 +0300

    triangeAreaCalculator practice done

--- Java/basicConcepts-Variables/practices/triangleAreaCalculator.java
+++ Java/basicConcepts-Variables/practices/triangleAreaCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class triangeAreaCalculator {
+public class triangleAreaCalculator {
     public static void main(String[] args) {
         /*
          * Homework
