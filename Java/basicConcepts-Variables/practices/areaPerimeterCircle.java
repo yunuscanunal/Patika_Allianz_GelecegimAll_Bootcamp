72071a850899148f3e3e1cd07c7ced4dec3dee0c
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-09 12:53:44 +0300

    area and perimeter calculator of circle done

---
+++ Java/basicConcepts-Variables/practices/areaPerimeterCircle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class areaPerimeterCircle {
+    public static void main(String[] args) {
+        /*
+         * Program to Calculate Area and Perimeter of a Circle
+         * Write a program in Java that calculates the area and perimeter of a circle
+         * whose radius you get from the user.
+         * 
+         * Area Formula: π * r * r;
+         * 
+         * Perimeter Formula: 2 * π * r;
+         */
+        int r;
+        double pi = 3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius: ");
+        r = input.nextInt();
+
+        double perimeter = 2 * pi * r;
+        double area = pi * r * r;
+
+        System.out.println("Area of the Circle: " + area + "\nPerimeter of the Circle: " + perimeter);
+    }
+}
