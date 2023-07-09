d7c5e2d3ce30dec252ab7ee248e0876437d72838
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-09 12:07:28 +0300

    triangeAreaCalculator practice done

---
+++ Java/basicConcepts-Variables/practices/triangeAreaCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class triangeAreaCalculator {
+    public static void main(String[] args) {
+        /*
+         * Homework
+         * Write a program that calculates the area of the triangle whose three side
+         * lengths you get from the user.
+         * 
+         * Formula
+         * perimeter of triangle = 2𝑢
+         * 
+         * 𝑢 = (a+b+c) / 2
+         * 
+         * Area * Area = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+         */
+
+        double a, b, c, u, area;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first side: ");
+        a = input.nextInt();
+
+        System.out.print("Enter second side: ");
+        b = input.nextInt();
+
+        System.out.print("Enter third side: ");
+        c = input.nextInt();
+
+        u = (a + b + c) / 2; // 2u = perimeter, u = perimeter/2
+        area = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+        System.out.println(area);
+
+    }
+
+}
