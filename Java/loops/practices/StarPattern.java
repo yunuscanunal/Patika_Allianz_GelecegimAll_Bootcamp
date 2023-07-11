23acaf0082babf9a9b934e6fe037a480a2f98aa1
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-11 14:37:41 +0300

    star pattern practice done

---
+++ Java/loops/practices/StarPattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        // *
+        // ***
+        // *****
+        // *******
+        // *********
+        // ***********
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
