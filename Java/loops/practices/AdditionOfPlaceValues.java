25232fcfba52fd3d8c544cfa72352055ba9e332a
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-11 14:16:39 +0300

    sum of place values practice done

--- Java/loops/practices/AdditionOfPlaceValues.java
+++ Java/loops/practices/AdditionOfPlaceValues.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class AdditionOfPlaceValues {
     public static void main(String[] args) {
-
+        /*
+         * Write a program that calculates the sum of the digits of a number.
+         * 
+         * Example: 1643 = 1 + 6 + 4 + 3 = 14
+         */
         Scanner input = new Scanner(System.in);
 
         System.out.print("Sayı Giriniz :");
