fc5a716f34c227ca1f678bf8b4e1c8e8dae3c15e
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-12 01:30:45 +0300

    fibonacci calculator done

---
+++ Java/loops/practices/Fibonacci.java
@@ -0,0 +1,22 @@
+package Java.loops.practices;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+
+        int n = input.nextInt();
+        System.out.println(fib(n));
+    }
+}
