c26557b8add9e7690f3ed776f5e351ae7f9c970b
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-11 14:53:08 +0300

    star pattern practice done

---
+++ Java/loops/practices/DiamondPattern.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        // *
+        // ***
+        // *****
+        // *******
+        // *********
+        // ***********
+        // *********
+        // *******
+        // *****
+        // ***
+        // *
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
--- Java/loops/practices/StarPattern.java
+++ Java/loops/practices/StarPattern.java
@@ -12,11 +12,11 @@ public class StarPattern {
         System.out.print("Bir Sayı Giriniz :");
         int n = klavye.nextInt();
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < (n - i); j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= (2 * i + 1); k++) {
+            for (int k = 1; k <= ((2 * i) - 1); k++) {
                 System.out.print("*");
             }
             System.out.println(" ");
