4fe327f03237594be4304c315b30250f70e25f12
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-17 01:05:03 +0300

    components fixed

--- Angular/BlogPost-Project-Angular/src/app/components/button/button.ts
+++ Angular/BlogPost-Project-Angular/src/app/components/button/button.ts
@@ -1,5 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output
+} from '@angular/core';
 
 @Component({
     selector: 'app-button',
