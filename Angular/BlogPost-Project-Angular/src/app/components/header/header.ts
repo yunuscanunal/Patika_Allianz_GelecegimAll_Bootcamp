6405422059ffe30623b8fdb6f31020828660bd9e
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-16 13:40:28 +0300

    header added

---
+++ Angular/BlogPost-Project-Angular/src/app/components/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.html',
+    styleUrls: ['./header.css']
+})
+export class HeaderComponent {
+
+}
