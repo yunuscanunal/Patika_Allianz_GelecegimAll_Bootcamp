2e0e735d28d1113312ab52f1d37e7cfb4f914c98
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-17 01:05:21 +0300

    app routing module added

--- Angular/BlogPost-Project-Angular/src/app/app-routing.module.ts
+++ Angular/BlogPost-Project-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './pages/user/user-list/user-list.component';
+import { PostListComponent } from './pages/post/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "userlist", component: UserListComponent },
+  { path: "postlist", component: PostListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
