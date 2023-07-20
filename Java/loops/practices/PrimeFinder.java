010e624bad384476ceff921713f546c124f1ab77
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-20 23:50:00 +0300

    prime finder practice done

---
+++ Java/loops/practices/PrimeFinder.java
@@ -0,0 +1,21 @@
+public class PrimeFinder {
+    public static void main(String[] args) {
+
+        System.out.println("1 - 100 arasındaki Asal Sayılar:");
+
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            boolean asalMi = true;
+
+            for (int i = 2; i <= sayi / 2; i++) {
+                if (sayi % i == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            if (asalMi) {
+                System.out.print(sayi + " ");
+            }
+        }
+    }
+}
