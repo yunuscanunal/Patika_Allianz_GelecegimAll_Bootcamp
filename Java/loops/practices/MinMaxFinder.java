e271af20af30cd8383c1afe3d79544fcdf74f413
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-20 23:41:44 +0300

    min max finder practice done

--- Java/loops/practices/MinMaxFinder.java
+++ Java/loops/practices/MinMaxFinder.java
@@ -1,3 +1,27 @@
-public class MinMaxFinder {
+import java.util.Scanner;
 
+public class MinMaxFinder {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Length of Array: ");
+        int timesOfLoop = input.nextInt();
+        int i = 0, minNumber = 0, maxNumber = 0;
+
+        while (i < timesOfLoop) {
+            System.out.print("Enter the " + (i + 1) + "'s Number: ");
+            int element = input.nextInt();
+            if (element < minNumber) {
+                minNumber = element;
+            }
+            if (element > maxNumber) {
+                maxNumber = element;
+            }
+            i++;
+        }
+
+        System.out.println("Minimum Sayı " + minNumber);
+        System.out.println("Maximum Sayı " + maxNumber);
+
+    }
 }
