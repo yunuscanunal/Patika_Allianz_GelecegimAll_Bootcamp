20f8d359a5f5ad40a430306e247e49664271481f
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-20 22:11:42 +0300

    frequencies practice done

---
+++ Java/Array/practices/Frequencies.java
@@ -0,0 +1,30 @@
+public class Frequencies {
+    public static void main(String[] args) {
+        // Initialize array
+        int[] arr = new int[] { 10, 20, 20, 10, 10, 20, 5, 20 };
+        // Array fr will store frequencies of element
+        int[] fr = new int[arr.length];
+        int visited = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    // To avoid counting same element again
+                    fr[j] = visited;
+                }
+            }
+            if (fr[i] != visited)
+                fr[i] = count;
+        }
+        // Displays the frequency of each element present in array
+        System.out.println("---------------------------------------");
+        System.out.println(" Element | Frequency");
+        System.out.println("---------------------------------------");
+        for (int i = 0; i < fr.length; i++) {
+            if (fr[i] != visited)
+                System.out.println("    " + arr[i] + "    |    " + fr[i]);
+        }
+        System.out.println("----------------------------------------");
+    }
+}
