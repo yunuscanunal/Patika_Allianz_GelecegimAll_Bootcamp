21559b6e7e79b522bc20d96054b614b69172058b
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-24 08:25:28 +0300

    gradeTest file main method done

--- Java/Homeworks/Homework3/src/util/GradeTest.java
+++ Java/Homeworks/Homework3/src/util/GradeTest.java
@@ -1,4 +1,10 @@
 package util;
 
 public class GradeTest {
+    public static void main(String[] args) {
+
+        for(Grade i: Grade.values()){
+            System.out.println("Value: " + i);
+        }
+    }
 }
