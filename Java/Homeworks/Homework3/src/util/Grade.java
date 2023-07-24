78aa7b3dbac189a1285fd25f44d2e795ca812c95
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-24 08:23:23 +0300

    grade enum done

--- Java/Homeworks/Homework3/src/util/Grade.java
+++ Java/Homeworks/Homework3/src/util/Grade.java
@@ -1,4 +1,28 @@
 package util;
 
 public enum Grade {
+    A("A", 4), B("B", 3), C("C", 2), D("D", 1), F("F", 0);
+
+    int numericValue;
+    String stringValue;
+    private Grade(String stringValue, int numericValue) {
+        this.stringValue = stringValue;
+        this.numericValue = numericValue;
+    }
+
+    public int getNumericValue() {
+        return numericValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "numericValue=" + numericValue +
+                ", stringValue='" + stringValue + '\'' +
+                '}';
+    }
 }
