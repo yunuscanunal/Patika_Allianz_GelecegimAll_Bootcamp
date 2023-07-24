33565ce2695c6f79c808f65257f19dbfe554496e
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-24 08:33:23 +0300

    transcript updated

--- Java/Homeworks/Homework3/src/main/Transcript.java
+++ Java/Homeworks/Homework3/src/main/Transcript.java
@@ -1,4 +1,28 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Transcript {
+    int StudentId;
+    ArrayList<CourseGrade> courseGradeArrayList;
+    double GPA;
+
+    public Transcript(int StudentId) {
+        this.StudentId = StudentId;
+        this.GPA = 0.0;
+        this.courseGradeArrayList = new ArrayList<>();
+    }
+
+    public void addTakenCourse(CourseGrade courseGrade) {
+        if (courseGrade != null) {
+            this.courseGradeArrayList.add(courseGrade);
+            double totalGrade = 0;
+            for (CourseGrade courseGrade1 : this.courseGradeArrayList) {
+                totalGrade += (double) courseGrade1.getGradeTaken().getNumericValue() / (double) this.courseGradeArrayList.size();
+            }
+            this.GPA = totalGrade;
+        } else {
+            System.out.println("CourseGrade cannot be null!");
+        }
+    }
 }
