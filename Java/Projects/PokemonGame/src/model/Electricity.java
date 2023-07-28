efba8c9c17c787c62d03a666f50ad3daeddf76b2
Yunuscan Ünal <yunuscanunal1@gmail.com>
2023-07-28 19:33:21 +0300

    typeEnum and file name changed

---
+++ Java/Projects/PokemonGame/src/model/Electricity.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Electricity extends SpecialPower{
+    public Electricity(String name, int extraDamage, int remainingRights) {
+        super(name, extraDamage, remainingRights);
+    }
+}
